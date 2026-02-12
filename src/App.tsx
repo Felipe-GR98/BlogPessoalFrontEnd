e397e5d05d70414c10da7682ed34939b1ff420eb
Felipe Gonçalves <felipegr2000@gmail.com>
2026-02-12 10:44:12 -0300

    feature/ Paginas de login e cadastro feitas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/foorter/Footer'
 import Navbar from './components/navbar/Navbar'
+import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
   return(
     <>
+    <BrowserRouter>
     <Navbar/>
-    <Home/>
+    <div className='min-h-[80vh]'>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/cadastro' element={<Cadastro/>}/>
+      </Routes>
+    </div>
     <Footer/>
+    </BrowserRouter>
     </>
 )
 }
